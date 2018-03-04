cf04544991f025bc729bb28fc18d3886bccfe5a1
Shai James <skyandbeyond@gmail.com>
2018-03-04 03:07:27 +0200

    typo

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -5,6 +5,26 @@
         <landing-hero></landing-hero>
 
         <section class="max-1024 pad-sides">
+
+
+            <section class="meta">
+
+                <section class="circle">
+                    <h1>X</h1>
+                    <h2>Identities Reserved</h2>
+                    <p>13,926,000+ tokens airdropped</p>
+                    <p class="open-sans" style="margin-top:-5px;">( and counting )</p>
+                </section>
+
+                <section class="ctas">
+                    <a href="https://docs.google.com/document/d/1P6Oz08vNjrJO1H3HmBkuh4BIVm3-0YAe4nqo-xfT4v0/edit?usp=sharing" target="_blank">
+                        <rounded-button big="More About RIDL" small="Reputation and Identity Layer"></rounded-button>
+                    </a>
+                    <a href="http://www.scatter-eos.com" target="_blank">
+                        <rounded-button big="More About Scatter" small="Already a working product."></rounded-button>
+                    </a>
+                </section>
+            </section>
             <section class="readme" ref="readme">
                 <h1>
                     Get Ready for some Action
@@ -20,22 +40,25 @@
                 </p>
                 <hr>
 
-                <h2>Secondary Airdrop</h2>
-                <p>
-                    Reserved User Identity Names will also be airdropped an additional 50 RIDL Tokens, raising their total to 100 ( 10 from registration, 40 from global airdrop and 50
-                    from reservation airdrop ).
-                </p>
-                <hr>
-
                 <h2>Identity Name Reservation</h2>
                 <p>
                     If you want to make sure that you get the name you want at the launch of the EOS mainnet or if you simply want to take part in the name auction, you will be able to reserve a
                     name and claim it once the auction opens. User Identity Names will be transferable until June 1st at which point they will be locked to the EOS public key they are tied to at the time.
                     <br><br>
                     <b>Applications</b> must prove their ownership of the application name via real world means in order to reserve the name due to copyright and intellectual property laws. If an
-                    application reserves a name that is already taken by a user the user’s EOS will be returned to them and the application will assume ownership of the name at
+                    application reserves a name that is already taken by a user and then verified successfully the user’s EOS will be returned to them and the application will assume ownership of the name at
                     the base price. Application Identities are non-transferable and can not take part in the auction system. Applications always start with 0 RIDL Tokens. If an
-                    application has tokens allocated to it’s public key when it is registered they will not be given to the Application Identity.
+                    application has tokens allocated to it’s public key when it is registered they will not be given to the Application Identity, but can still be claimed by User Identities
+                    registered to the same EOS public key.
+                    <br><br>
+                    <b>Reserving an Identity name costs 1 EOS.</b>
+                </p>
+                <hr>
+
+                <h2>Secondary Airdrop</h2>
+                <p>
+                    Reserved User Identity Names will also be airdropped an additional 50 RIDL Tokens, raising their total to 100 ( 10 from registration, 40 from global airdrop and 50
+                    from reservation airdrop ).
                 </p>
                 <hr>
 
@@ -46,7 +69,16 @@
                     You can bid on already reserved Identity Names and
                     if the owner selects to sell to your bid you will be transferred ownership of the name. If a new bid overthrows yours your bid will be returned to you.
                     All gas spent on bidding is non-refundable.
+
                 </p>
+
+                <figure class="bounding-box">
+                    <p style="margin:0;">
+                        <b>All bids are also done in EOS.</b> The reasoning for this being that we have a feeling that this auction will break through the bubble of EOS and flow into
+                        the general cryptocurrency world. Once that happens people who do not hold EOS will have to purchase EOS to take part in the auction. Like everything
+                        Scatter does, this will help elevate the entire EOS project, it's community, and it's exposure.
+                    </p>
+                </figure>
                 <hr>
 
                 <h2>Identity Genetics</h2>
@@ -67,24 +99,31 @@
 
                     </p>
                 </section>
-            </section>
+                <hr>
 
-            <section class="meta">
+                <h2>What will the proceeds from the reservations go towards?</h2>
+                <p>
+                    <b>Every penny will go right back into the project and fund further development of Scatter and RIDL.</b>
+                    This includes but is not limited to hiring more developers and
+                    splitting the development teams into three, <i>Desktop, Mobile, and RIDL.</i>
+                    <br>
+                    <br>
+                    A large portion will also be put into educating developers on
+                    how to work with EOS, eosjs, Scatter, and RIDL via documentation and video/written tutorials.
+                </p>
+                <hr>
 
-                <section class="ctas">
-                    <a href="https://docs.google.com/document/d/1P6Oz08vNjrJO1H3HmBkuh4BIVm3-0YAe4nqo-xfT4v0/edit?usp=sharing" target="_blank">
-                        <rounded-button big="More About RIDL" small="Reputation and Identity Layer"></rounded-button>
-                    </a>
-                    <a href="http://www.scatter-eos.com" target="_blank">
-                        <rounded-button big="More About Scatter" small="Already a working product."></rounded-button>
-                    </a>
-                </section>
-
-                <section class="circle">
-                    <h1>X</h1>
-                    <h2>Identities Reserved</h2>
-                    <p>13,926,000+ tokens airdropped</p>
-                </section>
+                <h2>This is <u>NOT</u> and Initial Coin Offering</h2>
+                <p>
+                    In no way is this an ICO. No tokens are being sold or even recorded on the Ethereum chain. The tokens will only be minted on the actual EOS network, and
+                    they will only be applied to the EOS public keys ( from genesis and the airdrop contract ) once that happens.
+                    The tokens currently have no price or value attached to them simply due to the fact that they do not exist on any network.
+                    <b>Any RIDL tokens on the Ethereum blockchain are <u>NOT</u> endorsed by Scatter and have nothing to do with us or this airdrop/auction.</b>
+                    The RIDL Token is a <b>utility</b> token. Though it <u>can</u> be, it is not Scatter's intention for the token to be sold and/or traded.
+                    <br><br>
+                    The only thing being sold here on this website is an actual virtual asset in the form of a reserved name for the future EOS blockchain.
+                    You may liken the reservation process to that of pre-purchasing a video game, book, or concert tickets.
+                </p>
             </section>
         </section>
 
@@ -123,6 +162,17 @@
 </script>
 
 <style lang="scss">
+    .bounding-box {
+        cursor: pointer;
+        text-align:left;
+        border:1px solid #333455;
+        border-radius:4px;
+        box-shadow:0 2px 4px rgba(0,0,0,0.1);
+        padding:20px;
+        width:100%;
+        margin-bottom:10px;
+        position: relative;
+    }
     .readme {
         margin:100px 0;
         color:#fff;
